b7b7cac8ba5641a09d550f238d1d793d45342fbf
Hadrien Lanneau <hadrien@lanneau.me>
2021-12-17 19:28:52 +0000

    Fix running build server

--- services/console/pages/_middleware.ts
+++
@@ -1,5 +0,0 @@
-import type { NextFetchEvent, NextRequest } from "next/server";
-
-export function middleware(req: NextRequest, ev: NextFetchEvent) {
-  if (process.env.NODE_ENV !== "development") return new Response("");
-}
