6aac0402019db769ce9aa0654b0bbd3e86c36ca2
Florian Peyron <5094617-florianpeyron@users.noreply.gitlab.com>
2021-12-31 10:30:06 +0000

    Console properly use .env variables at runtime

--- services/console/next.config.js
+++ services/console/next.config.js
@@ -4,4 +4,8 @@ const { i18n } = require("./next-i18next.config");
 module.exports = {
   reactStrictMode: true,
   i18n,
+  publicRuntimeConfig: {
+    API_HOST: process.env.API_HOST,
+    ENDPOINT: process.env.ENDPOINT,
+  },
 };
