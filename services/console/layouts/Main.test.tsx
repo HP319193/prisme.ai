6d120e9c5624a96714820dc8ec26fd56ac07527a
Hadrien Lanneau <hadrien@lanneau.me>
2022-01-04 10:14:36 +0000

    Logout

--- services/console/layouts/Main.test.tsx
+++ services/console/layouts/Main.test.tsx
@@ -5,10 +5,3 @@ it("should render", () => {
   const root = renderer.create(<Main />);
   expect(root.toJSON()).toMatchSnapshot();
 });
-
-it("should render with custom header", () => {
-  const root = renderer.create(
-    <Main header={() => <div>Custom header</div>} />
-  );
-  expect(root.toJSON()).toMatchSnapshot();
-});
