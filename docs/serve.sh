5b0be0719e1b4177970badf2346c0cdb78fd7945
florian peyron <florian.peyron@gogowego.com>
2022-01-18 15:23:42 +0100

    Added docs boilerplates

---
+++ docs/serve.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+serve() {
+  tmp_config=./.$1_mkdocs.yml
+  if ! (./build.sh config $1 $tmp_config) ; then
+    return 1
+  fi
+  mkdocs serve -f $tmp_config
+  rm $tmp_config
+}
+
+usage() {
+  echo "Usage :"
+  echo "    ./serve fr"
+  echo "    ./serve en"
+}
+if [ -z $1 ] ; then
+  echo Missing lang
+  usage
+else
+  serve $1
+fi
+
+
