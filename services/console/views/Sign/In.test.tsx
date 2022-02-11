239a513dadee17b845dfc47dc1d95ca35f479e53
florian peyron <florian.peyron@gogowego.com>
2022-02-11 10:25:54 +0100

    Revert "Text CI without blockin In.test.tsx"
    This reverts commit 25ef09d489eca0d67ef33de6ed9d000c2d780b65.

--- services/console/views/Sign/In.test.tsx
+++ services/console/views/Sign/In.test.tsx
@@ -5,100 +5,98 @@ import ApiError from '../../api/ApiError';
 import { Form } from 'react-final-form';
 import { useRouter } from 'next/router';
 
-it('should fake a test', () => {});
+jest.mock('../../components/UserProvider', () => {
+  const mock: any = {};
+  mock.mock = mock;
+  return {
+    useUser: () => mock,
+  };
+});
+jest.mock('next/router', () => {
+  const push = jest.fn();
+  return {
+    useRouter: () => ({
+      push,
+    }),
+  };
+});
+jest.mock('next/Image', () => {
+  return ({ src }: any) => <div>{src}</div>;
+});
 
-// jest.mock('../../components/UserProvider', () => {
-//   const mock: any = {};
-//   mock.mock = mock;
-//   return {
-//     useUser: () => mock,
-//   };
-// });
-// jest.mock('next/router', () => {
-//   const push = jest.fn();
-//   return {
-//     useRouter: () => ({
-//       push,
-//     }),
-//   };
-// });
-// jest.mock('next/Image', () => {
-//   return ({ src }: any) => <div>{src}</div>;
-// });
-//
-// jest.mock('primereact/fieldset', () => ({
-//   Fieldset: ({ children }: any) => children,
-// }));
-//
-// beforeEach(() => {
-//   const { mock } = useUser() as any;
-//   mock.user = null;
-//   mock.loading = false;
-//   mock.error = null;
-//   mock.signin = jest.fn();
-// });
-//
-// it('should render', () => {
-//   const root = renderer.create(<SignIn />);
-//   expect(root.toJSON()).toMatchSnapshot();
-// });
-//
-// it('should load', () => {
-//   (useUser() as any).mock.loading = true;
-//   const root = renderer.create(<SignIn />);
-//   expect(root.toJSON()).toMatchSnapshot();
-// });
-//
-// it('should have error', () => {
-//   (useUser() as any).mock.error = new ApiError(
-//     {
-//       error: 'Error',
-//       message: 'Error',
-//     },
-//     400
-//   );
-//   const root = renderer.create(<SignIn />);
-//   expect(root.toJSON()).toMatchSnapshot();
-// });
-//
-// it('should submit form', async () => {
-//   const root = renderer.create(<SignIn />);
-//   await act(async () => {
-//     await root.root
-//       .findByType(Form)
-//       .props.onSubmit({ email: 'email@company.com', password: '123456' });
-//   });
-//   expect(useUser().signin).toHaveBeenCalledWith('email@company.com', '123456');
-// });
-//
-// it('should validate form', async () => {
-//   const root = renderer.create(<SignIn />);
-//   expect(
-//     root.root.findByType(Form).props.validate({ email: '', password: '' })
-//   ).toEqual({
-//     email: 'required',
-//     password: 'required',
-//   });
-//   expect(
-//     root.root.findByType(Form).props.validate({ email: 'a', password: '' })
-//   ).toEqual({
-//     password: 'required',
-//   });
-//   expect(
-//     root.root.findByType(Form).props.validate({ email: '', password: 'a' })
-//   ).toEqual({
-//     email: 'required',
-//   });
-//   expect(
-//     root.root.findByType(Form).props.validate({ email: 'a', password: 'a' })
-//   ).toEqual({});
-// });
-//
-// it('should redirect after signin', () => {
-//   const root = renderer.create(<SignIn />);
-//   act(() => {
-//     (useUser() as any).mock.user = {};
-//     root.update(<SignIn />);
-//   });
-//   expect(useRouter().push).toHaveBeenCalledWith('/workspaces');
-// });
+jest.mock('primereact/fieldset', () => ({
+  Fieldset: ({ children }: any) => children,
+}));
+
+beforeEach(() => {
+  const { mock } = useUser() as any;
+  mock.user = null;
+  mock.loading = false;
+  mock.error = null;
+  mock.signin = jest.fn();
+});
+
+it('should render', () => {
+  const root = renderer.create(<SignIn />);
+  expect(root.toJSON()).toMatchSnapshot();
+});
+
+it('should load', () => {
+  (useUser() as any).mock.loading = true;
+  const root = renderer.create(<SignIn />);
+  expect(root.toJSON()).toMatchSnapshot();
+});
+
+it('should have error', () => {
+  (useUser() as any).mock.error = new ApiError(
+    {
+      error: 'Error',
+      message: 'Error',
+    },
+    400
+  );
+  const root = renderer.create(<SignIn />);
+  expect(root.toJSON()).toMatchSnapshot();
+});
+
+it('should submit form', async () => {
+  const root = renderer.create(<SignIn />);
+  await act(async () => {
+    await root.root
+      .findByType(Form)
+      .props.onSubmit({ email: 'email@company.com', password: '123456' });
+  });
+  expect(useUser().signin).toHaveBeenCalledWith('email@company.com', '123456');
+});
+
+it('should validate form', async () => {
+  const root = renderer.create(<SignIn />);
+  expect(
+    root.root.findByType(Form).props.validate({ email: '', password: '' })
+  ).toEqual({
+    email: 'required',
+    password: 'required',
+  });
+  expect(
+    root.root.findByType(Form).props.validate({ email: 'a', password: '' })
+  ).toEqual({
+    password: 'required',
+  });
+  expect(
+    root.root.findByType(Form).props.validate({ email: '', password: 'a' })
+  ).toEqual({
+    email: 'required',
+  });
+  expect(
+    root.root.findByType(Form).props.validate({ email: 'a', password: 'a' })
+  ).toEqual({});
+});
+
+it('should redirect after signin', () => {
+  const root = renderer.create(<SignIn />);
+  act(() => {
+    (useUser() as any).mock.user = {};
+    root.update(<SignIn />);
+  });
+  expect(useRouter().push).toHaveBeenCalledWith('/workspaces');
+});
