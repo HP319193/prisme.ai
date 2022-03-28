eb2fde74d6a4ac15aba95bdd7c2d66fe5df4876d
Hadrien Lanneau <hadrien.lanneau@gogowego.com>
2022-03-28 11:03:02 +0200

    Input description

--- services/console/components/SchemaFormBuilder/ArgumentsEditor.tsx
+++ services/console/components/SchemaFormBuilder/ArgumentsEditor.tsx
@@ -1,3 +1,5 @@
+import { InfoCircleOutlined } from '@ant-design/icons';
+import { Tooltip } from 'antd';
 import { useTranslation } from 'next-i18next';
 import Properties from './Properties';
 
@@ -6,7 +8,17 @@ export const ArgumentsEditor = ({ value, onChange }: any) => {
 
   return (
     <div className="ant-input">
-      <div className="text-gray pl-4">{t('automations.arguments.title')}</div>
+      <div className="text-gray pl-4 flex flex-1 flex-row justify-between">
+        {t('automations.arguments.title')}
+        <div className="text-accent mr-2">
+          <Tooltip
+            title={t('automations.arguments.description')}
+            placement="left"
+          >
+            <InfoCircleOutlined />
+          </Tooltip>
+        </div>
+      </div>
       <Properties value={value} onChange={onChange} />
     </div>
   );
