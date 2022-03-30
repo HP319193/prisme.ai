e0f0c4ce998fe2505bcdc0573141bb3c2976256f
Hadrien Lanneau <hadrien.lanneau@gogowego.com>
2022-03-30 17:18:41 +0200

    Remove debug

--- services/console/utils/debug.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-export const addDebug = (name: string, fn: Function) => {
-  window.Prisme = window.Prisme || {};
-  window.Prisme.ai = window.Prisme.ai || {};
-  window.Prisme.ai[name] = fn;
-};
-
-export default addDebug;
--- services/console/views/Workspace.tsx
+++ services/console/views/Workspace.tsx
@@ -19,24 +19,13 @@ import PagesSidebar from './PagesSidebar';
 import { DoubleLeftOutlined, DoubleRightOutlined } from '@ant-design/icons';
 import ShareWorkspace from '../components/Share/ShareWorkspace';
 import Storage from '../utils/Storage';
-import addDebug from '../utils/debug';
 
 export const Workspace = () => {
   const { t } = useTranslation('workspaces');
-  const {
-    workspace,
-    fullSidebar,
-    setFullSidebar,
-    setShare,
-    save,
-  } = useWorkspace();
+  const { workspace, fullSidebar, setFullSidebar, setShare } = useWorkspace();
   const [sidebar, setSidebar] = useState(
     Storage.get('__workpaceSidebar') || 'automations'
   );
-
-  addDebug('setWorkspaceSlug', (slug: string) => {
-    save({ ...workspace, slug });
-  });
 
   useEffect(() => {
     Storage.set('__workpaceSidebar', sidebar);
