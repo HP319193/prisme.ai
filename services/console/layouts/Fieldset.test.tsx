3f546e433f138b13ffea34f7fe6021dbbd05f075
Hadrien Lanneau <hadrien.lanneau@gogowego.com>
2022-04-20 16:09:15 +0200

    Check types on CI

--- services/console/layouts/Fieldset.test.tsx
+++ services/console/layouts/Fieldset.test.tsx
@@ -2,6 +2,6 @@ import Fieldset from './Fieldset';
 import renderer from 'react-test-renderer';
 
 it('should render', () => {
-  const root = renderer.create(<Fieldset />);
+  const root = renderer.create(<Fieldset legend="foo">bar</Fieldset>);
   expect(root.toJSON()).toMatchSnapshot();
 });
