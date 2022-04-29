8461162ba527e077992713505b640f549e19eafb
Hadrien Lanneau <hadrien.lanneau@gogowego.com>
2022-04-29 16:26:15 +0200

    Ability to choose to set a raw value instead of required type

--- services/console/layouts/WorkspaceLayout/WorkspaceLayout.tsx
+++ services/console/layouts/WorkspaceLayout/WorkspaceLayout.tsx
@@ -423,7 +423,7 @@ export const WorkspaceLayout: FC = ({ children }) => {
           transition-transform
           transition-duration-200
           transition-ease-in
-          z-10
+          z-[11]
           ${displaySourceView ? '' : '-translate-y-full'}
         `}
       >
