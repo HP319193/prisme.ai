8a0a238e963bf88cdaf5ba6602566d1c73fd0db0
Hadrien Lanneau <hadrien@lanneau.me>
2022-05-03 08:25:43 +0000

    Remove debug

--- services/console/components/PagesProvider/PagesProvider.tsx
+++ services/console/components/PagesProvider/PagesProvider.tsx
@@ -34,7 +34,6 @@ export const PagesProvider: FC<PagesProvider> = ({ children }) => {
   );
   const savePage: PagesContext['savePage'] = useCallback(
     async (workspaceId, page) => {
-      console.log(page);
       const savedPage = await api.updatePage(workspaceId, page);
       setPages((prev) => {
         const newPages = new Map(prev);
