328441bc656689a3dd802ecfd601d21761c57d00
martin <martin.caussanel@prisme.ai>
2022-05-03 15:50:10 +0200

    Fetch instruction now automatically adds content-type: application/json if not specified. Now handle application/x-www-form-urlencoded body content-type.

--- services/runtime/src/services/runtime/automations/instructions/fetch.ts
+++ services/runtime/src/services/runtime/automations/instructions/fetch.ts
@@ -1,11 +1,31 @@
 import nodeFetch, { RequestInit } from 'node-fetch';
 import { ContextsManager } from '../../contexts';
 import { CORRELATION_ID_HEADER } from '../../../../../config';
+import { URLSearchParams } from 'url';
 
 export async function fetch(
   { url, body, headers, method }: Prismeai.Fetch['fetch'],
   ctx: ContextsManager
 ) {
+  const lowercasedHeaders: Record<string, string> = Object.entries(
+    headers || {}
+  )
+    .map(([key, value]) => [key.toLowerCase(), value])
+    .reduce(
+      (acc, [key, value]) => ({
+        ...acc,
+        [key]: value,
+      }),
+      {}
+    );
+
+  if (!lowercasedHeaders['content-type'] && body) {
+    headers = {
+      ...headers,
+      'content-type': 'application/json',
+    };
+  }
+
   const params: RequestInit = {
     headers: {
       [CORRELATION_ID_HEADER]: ctx.run.correlationId,
@@ -14,7 +34,16 @@ export async function fetch(
     method: method,
   };
   if (body && (method || 'get')?.toLowerCase() !== 'get') {
-    params.body = typeof body === 'object' ? JSON.stringify(body) : body;
+    if (
+      lowercasedHeaders['content-type'] &&
+      (lowercasedHeaders['content-type'] || '').includes(
+        'application/x-www-form-urlencoded'
+      )
+    ) {
+      params.body = typeof body === 'object' ? new URLSearchParams(body) : body;
+    } else {
+      params.body = typeof body === 'object' ? JSON.stringify(body) : body;
+    }
   }
   const result = await nodeFetch(url, params);
   if ((result.headers.get('Content-Type') || '').includes('application/json')) {
