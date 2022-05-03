0a3833f352b3b162840cf0e7736058c7a80fd5f7
martin <martin.caussanel@prisme.ai>
2022-05-03 15:03:43 +0200

    Fix workspaces build

--- services/workspaces/src/api/middlewares/validation.ts
+++ services/workspaces/src/api/middlewares/validation.ts
@@ -13,7 +13,7 @@ export const validationMiddleware = ({ ignorePaths = [] }: ValidationOpts) => {
   return OpenApiValidator.middleware({
     apiSpec: OPENAPI_FILEPATH,
     validateRequests: true,
-    validateResponses: false,
+    validateResponses: DEBUG,
     validateSecurity: false,
     ignorePaths: !ignorePaths.length
       ? undefined
