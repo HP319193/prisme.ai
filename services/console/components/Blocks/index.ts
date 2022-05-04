ca05f3774e3bc27256bfbcd6ccbf2cc3dc29bc86
Hadrien Lanneau <hadrien.lanneau@gogowego.com>
2022-05-04 16:35:13 +0200

    Block form

--- services/console/components/Blocks/index.ts
+++ services/console/components/Blocks/index.ts
@@ -1,3 +1,4 @@
 export { default as Title } from './Title';
 export { default as Cards } from './Cards';
 export { default as DataTable } from './DataTable';
+export { default as Form } from './Form';
