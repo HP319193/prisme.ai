7d0b4a0a8d739704791fc2bfd660086a0d0bc0d7
Florian Peyron <florian.peyron@gogowego.com>
2022-05-16 11:52:39 +0200

    Updated signin redirect to occur only for anonymous users on proper pages

--- services/console/views/Home.tsx
+++ services/console/views/Home.tsx
@@ -1,18 +1,10 @@
 import { NextPage } from 'next';
-import { useTranslation } from 'react-i18next';
-import { Button } from '@prisme.ai/design-system';
-import Link from 'next/link';
+import { Loading } from '@prisme.ai/design-system';
 
 export const Home: NextPage = () => {
-  const { t } = useTranslation('common');
   return (
-    <div className="flex justify-center align-center min-h-screen">
-      <main className="flex align-center justify-center flex-col">
-        <h1 className="flex">{t('main.title')}</h1>
-        <Button>
-          <Link href="/signin">{t('home.signin')}</Link>
-        </Button>
-      </main>
+    <div className="flex grow justify-center align-center min-h-screen">
+      <Loading />
     </div>
   );
 };
