a77e89ad35ccb3479da1d7e09c8d95937ff2a2e1
martin <martin.caussanel@prisme.ai>
2022-07-07 21:58:38 +0200

    Allow uploading file with its dataURI

--- services/workspaces/src/api/routes/files.ts
+++ services/workspaces/src/api/routes/files.ts
@@ -8,12 +8,19 @@ export default function init(fileStorage: FileStorage) {
     {
       context,
       body,
-      files,
+      files = [],
       params: { workspaceId },
       accessManager,
     }: Request<PrismeaiAPI.ListFiles.PathParameters, any>,
     res: Response<PrismeaiAPI.ListFiles.Responses.$200>
   ) {
+    for (const [key, value] of Object.entries(body)) {
+      if (typeof value === 'string' && value.startsWith('data:')) {
+        (files as any[]).push(parseDataURI(value));
+        delete body[key];
+      }
+    }
+
     const fileUploadRequests: FileUploadRequest[] = (
       (files as Express.Multer.File[]) || []
     ).map(({ size, originalname: name, buffer, mimetype }, idx) => {
@@ -114,4 +121,28 @@ export default function init(fileStorage: FileStorage) {
   app.get(`/:id`, asyncRoute(getFileHandler));
 
   return app;
+}
+
+function parseDataURI(uri: string) {
+  if (!uri.startsWith('data:')) {
+    return;
+  }
+  uri = uri.slice(5);
+  const split = uri.split(';').map((cur) => cur.trim());
+  if (split.length < 3) {
+    return;
+  }
+  const mimetype = split[0];
+  const originalname = split[1].startsWith('filename:')
+    ? split[1].slice(9)
+    : split[1];
+  const base64 = split[2].startsWith('base64,') ? split[2].slice(7) : split[2];
+
+  const buffer = Buffer.from(base64, 'base64');
+  return {
+    size: buffer.length,
+    originalname,
+    buffer,
+    mimetype,
+  };
 }
