bb675a2a617b8d47bcae0428a26a18640462117b
Florian Peyron <florian.peyron@gogowego.com>
2022-09-27 13:35:48 +0200

    Updated some public page styles for better custom colors support

--- packages/blocks/lib/Blocks/Internal/BlockTitle.tsx
+++ packages/blocks/lib/Blocks/Internal/BlockTitle.tsx
@@ -8,7 +8,10 @@ interface BlockTitleProps {
 const BlockTitle = ({ value }: BlockTitleProps) => {
   return (
     <div className={tw`block-cards__title-container title-container pt-8`}>
-      <Title level={4} className={`block-title ${tw`!text-[2.5rem]`}`}>
+      <Title
+        level={4}
+        className={`block-title ${tw`!text-[2.5rem] !text-theme-text`}`}
+      >
         {value}
       </Title>
     </div>
