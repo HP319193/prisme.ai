0dc1aedfe570fbec3c8412c3c75cb6b16607903f
martin <martin.caussanel@prisme.ai>
2022-09-28 15:07:17 +0200

    Fix retrieving user context from an automation triggered by event & with an unauthenticated session

--- services/runtime/src/services/runtime/index.ts
+++ services/runtime/src/services/runtime/index.ts
@@ -346,7 +346,7 @@ export default class Runtime {
     broker: Broker
   ) {
     const session = (await this.cache.getSession(source.sessionId)) || {
-      userId: source.userId!!,
+      userId: source.userId!! || source.sessionId!!, // For unauthenticated sessions, keep sessionId as a fake userId
       sessionId: source.sessionId!!,
       authData: {},
     };
