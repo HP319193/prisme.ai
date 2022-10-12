ef06b82b42958040cd6cde097e28af530e6ed376
Hadrien Lanneau <hadrien.lanneau@gogowego.com>
2022-10-12 12:39:36 +0200

    Add cards variants

--- services/console/components/PageBuilder/useBlocks.ts
+++ services/console/components/PageBuilder/useBlocks.ts
@@ -7,6 +7,54 @@ import workspaceIcon from '../../icons/icon-workspace.svg';
 
 const builtinBlocksVariants = [
   {
+    name: {
+      fr: 'Cartes style court',
+      en: 'Cards Short',
+    },
+    slug: 'Cards_Short',
+    block: 'Cards',
+    photo: '/images/blocks/preview-Cards-short.png',
+    config: {
+      variant: 'short',
+    },
+  },
+  {
+    name: {
+      fr: 'Cartes style article',
+      en: 'Cards Article',
+    },
+    slug: 'Cards_Article',
+    block: 'Cards',
+    photo: '/images/blocks/preview-Cards-articles.png',
+    config: {
+      variant: 'article',
+    },
+  },
+  {
+    name: {
+      fr: 'Cartes style carré',
+      en: 'Cards Square',
+    },
+    slug: 'Cards_Square',
+    block: 'Cards',
+    photo: '/images/blocks/preview-Cards-square.png',
+    config: {
+      variant: 'square',
+    },
+  },
+  {
+    name: {
+      fr: "Cartes d'actions",
+      en: 'Actions cards',
+    },
+    slug: 'Cards_Action',
+    block: 'Cards',
+    photo: '/images/blocks/preview-Cards-actions.png',
+    config: {
+      variant: 'actions',
+    },
+  },
+  {
     name: 'Contact Form',
     slug: 'Form_Contact',
     block: 'Form',
