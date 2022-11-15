8e1670527feba346ac65dba5552356bdf4f3e824
Hadrien Lanneau <hadrien.lanneau@gogowego.com>
2022-11-15 10:48:52 +0100

    Fix pages services with docker

--- services/pages/docker/start.sh
+++ services/pages/docker/start.sh
@@ -1,25 +1,4 @@
-#!/usr/bin/env node
+#!/bin/sh
 
-const fs = require('fs');
-
-const ENVS = ['API_HOST', 'PAGES_HOST', 'SENTRY_DSN', 'CONSOLE_HOST'];
-
-//services/pages/.next/server/pages/default/404.html
-function rewriteConfig() {
-  const errorFile = fs.readFileSync(
-    '/www/services/pages/.next/server/pages/default/404.html'
-  );
-
-  fs.writeFileSync(
-    '/www/services/pages/.next/server/pages/default/404.html',
-    ENVS.reduce(
-      (prev, env) =>
-        prev.replace(
-          new RegExp(`"${env}":"[^\"]*"`),
-          `"${env}":"${process.env[env] || 'changed'}"`
-        ),
-      `${errorFile}`
-    )
-  );
-}
-rewriteConfig();
+node services/pages/docker/start.js
+npm --prefix services/pages start
