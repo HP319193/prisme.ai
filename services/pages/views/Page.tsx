3693f3ad284e97b83cb6cae5b28b7be73e47dea8
Hadrien Lanneau <hadrien.lanneau@gogowego.com>
2022-11-28 10:59:26 +0100

    Fix auto upload files

--- services/pages/views/Page.tsx
+++ services/pages/views/Page.tsx
@@ -1,6 +1,5 @@
-import { useRouter } from 'next/router';
 import DefaultErrorPage from 'next/error';
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Loading } from '@prisme.ai/design-system';
 import SigninForm from '../../console/components/SigninForm';
 import api from '../../console/utils/api';
@@ -8,6 +7,7 @@ import PageRenderer, {
   PageProps as PageRendererProps,
 } from '../components/Page/Page';
 import { usePage } from '../components/Page/PageProvider';
+import { useWorkspace } from '../components/Workspace';
 
 export interface PageProps extends Omit<PageRendererProps, 'page'> {
   page: PageRendererProps['page'] | null;
@@ -15,6 +15,12 @@ export interface PageProps extends Omit<PageRendererProps, 'page'> {
 
 export const Page = ({ page: pageFromServer, error }: PageProps) => {
   const { page, setPage, loading, fetchPage } = usePage();
+  const { setId } = useWorkspace();
+
+  useEffect(() => {
+    if (!page || !page.workspaceId) return;
+    setId(page.workspaceId);
+  }, [page, setId]);
 
   useEffect(() => {
     setPage(pageFromServer, error);
