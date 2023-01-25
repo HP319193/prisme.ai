bfe78ffcdd4c34ee55cc3c798eda8e698eea57f2
Hadrien Lanneau <hadrien.lanneau@gogowego.com>
2023-01-25 16:01:40 +0100

    Buttons with css

--- packages/blocks/lib/Blocks/Buttons.stories.tsx
+++ packages/blocks/lib/Blocks/Buttons.stories.tsx
@@ -1,75 +1,55 @@
 import { Story } from '@storybook/react';
-import { useState } from 'react';
-import { BlockProvider, BlocksProvider } from '../Provider';
+import { BlockProvider } from '../Provider';
 import Buttons from './Buttons';
 
 export default {
   title: 'Blocks/Buttons',
 };
 
-const Template: Story<any> = ({ defaultConfig }) => {
-  const [config, setConfig] = useState<any>(defaultConfig);
-  const [appConfig, setAppConfig] = useState<any>();
-
+const Template: Story<any> = (config) => {
   return (
-    <BlocksProvider
-      components={{
-        Link: (props) => <a {...props} />,
-        Loading: () => null,
-        DownIcon: () => null,
-      }}
-      externals={{}}
-    >
-      <BlockProvider
-        config={config}
-        onConfigUpdate={setConfig}
-        appConfig={appConfig}
-        onAppConfigUpdate={setAppConfig}
-      >
-        <Buttons />
-      </BlockProvider>
-    </BlocksProvider>
+    <BlockProvider config={config}>
+      <Buttons />
+    </BlockProvider>
   );
 };
 
 export const Default = Template.bind({});
 Default.args = {
-  defaultConfig: {
-    buttons: [
-      {
-        text: 'Button1',
-        variant: 'default',
-        action: {
-          type: 'event',
-          value: 'button.je suis le pere noel',
-        },
+  buttons: [
+    {
+      text: 'Button1',
+      variant: 'default',
+      action: {
+        type: 'event',
+        value: 'button.je suis le pere noel',
       },
-      {
-        text: 'Button2',
-        variant: 'primary',
+    },
+    {
+      text: 'Button2',
+      variant: 'primary',
+    },
+    {
+      text: 'Mes messages',
+      variant: 'default',
+      tag: '50',
+    },
+    {
+      text: 'Archived messages',
+      variant: 'default',
+      tag: '50',
+      unselected: true,
+    },
+    {
+      text: 'my url',
+      variant: 'link',
+      action: {
+        type: 'url',
+        popup: true,
+        value: 'http://studio.prisme.ai',
       },
-      {
-        text: 'Mes messages',
-        variant: 'default',
-        tag: '50',
-      },
-      {
-        text: 'Archived messages',
-        variant: 'default',
-        tag: '50',
-        unselected: true,
-      },
-      {
-        text: 'my url',
-        variant: 'link',
-        action: {
-          type: 'url',
-          popup: true,
-          value: 'http://studio.prisme.ai',
-        },
-      },
-    ],
-  },
+    },
+  ],
 };
 
 export const SimpleButton = Template.bind({});
