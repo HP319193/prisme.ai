50b57f5978526a5f41799f787e8ca0cb550d2ff4
Hadrien Lanneau <hadrien.lanneau@gogowego.com>
2023-02-03 14:50:15 +0100

    uiwidget Block takes a onChange event to link value to the form

--- services/pages/components/BlocksProvider/SchemaForm.tsx
+++ services/pages/components/BlocksProvider/SchemaForm.tsx
@@ -1,10 +1,28 @@
+import { useBlock } from '@prisme.ai/blocks';
 import { SchemaForm as OriginalSchemaForm } from '@prisme.ai/design-system';
 import { FieldComponent } from '@prisme.ai/design-system/lib/Components/SchemaForm/context';
+import { useEffect } from 'react';
+import { useField } from 'react-final-form';
 import RichTextEditor from '../../../console/components/RichTextEditor';
 import BlockLoader from '../Page/BlockLoader';
 
-const BlockWidget: FieldComponent = ({ schema }) => {
-  const { ['ui:options']: { block: { slug, ...config } } = {} } = schema;
+const BlockWidget: FieldComponent = ({ schema, name }) => {
+  const {
+    ['ui:options']: { block: { slug, onChange, ...config } } = {},
+  } = schema;
+  const { events } = useBlock();
+  const field = useField(name);
+
+  useEffect(() => {
+    if (!onChange || !events) return;
+    const off = events.on(onChange, ({ payload }) => {
+      field.input.onChange(payload);
+    });
+    return () => {
+      off();
+    };
+  }, [onChange, events, field.input]);
+
   return <BlockLoader name={slug} config={config} />;
 };
 
