70a04259c67fcca886eda098eb0cbadf2922459c
Hadrien Lanneau <hadrien.lanneau@gogowego.com>
2023-02-17 17:22:40 +0100

    RichText can interpolate variables with mustache
    # Conflicts:
    #	packages/blocks/lib/Blocks/Card/Card.stories.tsx
    #	packages/blocks/lib/Blocks/Card/Card.tsx

--- packages/blocks/lib/Blocks/RichText.stories.tsx
+++ packages/blocks/lib/Blocks/RichText.stories.tsx
@@ -5,6 +5,7 @@ import { BlockProvider, BlocksProvider } from '../Provider';
 import RichText from './RichText';
 import { tw } from 'twind';
 import { PreviewInStory } from './PreviewInStory';
+import { BlockLoader } from '../BlockLoader';
 
 export default {
   title: 'Blocks/RichText',
@@ -21,19 +22,18 @@ const Loading = () => {
   return <div>wait for it</div>;
 };
 const DownIcon = () => null;
-const Template: Story<any> = ({ defaultConfig }) => {
-  const [config, setConfig] = useState<any>(defaultConfig);
-  const [appConfig, setAppConfig] = useState<any>();
-
+const SchemaForm = () => null;
+const Template: Story<any> = (config) => {
   return (
     <div className={tw`flex flex-1 flex-col justify-between`}>
-      <BlocksProvider components={{ Link, Loading, DownIcon }} externals={{}}>
-        <BlockProvider
-          config={config}
-          onConfigUpdate={setConfig}
-          appConfig={appConfig}
-          onAppConfigUpdate={setAppConfig}
-        >
+      <BlocksProvider
+        components={{ Link, Loading, DownIcon, SchemaForm }}
+        externals={{}}
+        utils={{
+          BlockLoader,
+        }}
+      >
+        <BlockProvider config={config}>
           <RichText />
           {RichText.Preview && <PreviewInStory Preview={RichText.Preview} />}
         </BlockProvider>
@@ -44,8 +44,7 @@ const Template: Story<any> = ({ defaultConfig }) => {
 
 export const Default = Template.bind({});
 Default.args = {
-  defaultConfig: {
-    content: `
+  content: `
 <h1>HTML</h1>
 <p>Hello <a href="https://prisme.ai">World</a></p>
 
@@ -53,43 +52,43 @@ Default.args = {
 
 This is some __markdown__ content with [link](https://prisme.ai).
 `,
-  },
 };
 
 export const OneTag = Template.bind({});
 OneTag.args = {
-  defaultConfig: {
-    content: `# Just a title`,
-  },
+  content: `# Just a title`,
 };
 
 export const NestedDivs = Template.bind({});
 NestedDivs.args = {
-  defaultConfig: {
-    content: `<div class="contacts" prout-truc="chose">
+  content: `<div class="contacts" prout-truc="chose">
                     <div class="contactElement">
                       Hello 
                     </div>
                     </div>
 `,
-  },
 };
 
 export const WithScript = Template.bind({});
 WithScript.args = {
-  defaultConfig: {
-    content: `<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+  content: `<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
 <script>alert(jQuery)</script>
 `,
-    allowScripts: true,
-  },
+  allowScripts: true,
 };
 
 export const WithLinks = Template.bind({});
 WithLinks.args = {
-  defaultConfig: {
-    content: `<a href="https://prisme.ai">Prisme.ai</a>
+  content: `<a href="https://prisme.ai">Prisme.ai</a>
 <a href="https://prisme.ai" target="_blank">Prisme.ai too</a>
 `,
+};
+
+export const WithTemplate = Template.bind({});
+WithTemplate.args = {
+  content: `<div>{{title}}</div><div>{{text}}</div>`,
+  values: {
+    title: 'The title',
+    text: 'The text',
   },
 };
