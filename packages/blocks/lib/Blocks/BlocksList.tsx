a1264056cd078822bdccfd6322e199449f4cf662
Florian Peyron <florian.peyron@gogowego.com>
2023-04-13 17:15:36 +0200

    Fix tabs view data lost on change

--- packages/blocks/lib/Blocks/BlocksList.tsx
+++ packages/blocks/lib/Blocks/BlocksList.tsx
@@ -2,6 +2,7 @@ import { useBlock } from '../Provider';
 import { useBlocks } from '../Provider/blocksContext';
 import { BaseBlock } from './BaseBlock';
 import { BaseBlockConfig } from './types';
+import { useMemo } from 'react';
 
 export interface BlocksListConfig extends BaseBlockConfig {
   blocks: ({ slug: string } & Record<string, any>)[];
@@ -13,23 +14,27 @@ export const BlocksList = ({ blocks = [], className }: BlocksListConfig) => {
     utils: { BlockLoader },
   } = useBlocks();
 
+  const memoizedBlocks = useMemo(() => {
+    return blocks
+      .filter(Boolean)
+      .map(({ slug, className = '', ...config }, key) => (
+        <BlockLoader
+          key={`${key}-${slug}`}
+          name={slug}
+          config={{
+            ...config,
+            parentClassName: `pr-block-blocks-list__block--${key}`,
+            className: `pr-block-blocks-list__block pr-block-blocks-list__block--${key} ${className}`,
+          }}
+        />
+      ));
+  }, [blocks]);
+
   if (!Array.isArray(blocks)) return null;
 
   return (
     <div className={`pr-block-blocks-list ${className ? className : ''}`}>
-      {blocks
-        .filter(Boolean)
-        .map(({ slug, className = '', ...config }, key) => (
-          <BlockLoader
-            key={`${key}-${slug}`}
-            name={slug}
-            config={{
-              ...config,
-              parentClassName: `pr-block-blocks-list__block--${key}`,
-              className: `pr-block-blocks-list__block pr-block-blocks-list__block--${key} ${className}`,
-            }}
-          />
-        ))}
+      {memoizedBlocks}
     </div>
   );
 };
