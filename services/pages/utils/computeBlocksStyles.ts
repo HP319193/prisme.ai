3faab1cd440a7464bf04f0a859daa6fa1a14a12f
Hadrien Lanneau <hadrien.lanneau@gogowego.com>
2023-04-26 15:29:40 +0200

    Add cssId on every Blocks in page

--- services/pages/utils/computeBlocksStyles.ts
+++ services/pages/utils/computeBlocksStyles.ts
@@ -2,33 +2,40 @@ import { getBlockStyles, builtinBlocks } from '@prisme.ai/blocks';
 
 export function computePageStyles(page: Prismeai.DetailedPage) {
   const styles: string[] = [];
+
+  function computeCss(o: any): any {
+    if (Array.isArray(o)) {
+      return o.map(computeCss);
+    }
+    if (typeof o === 'object') {
+      if (o.slug !== undefined) {
+        const slug = o.slug || '';
+        const { styles: defaultStyles } = (builtinBlocks as any)[slug] || {
+          styles: '',
+        };
+        o.cssId = parseInt(`${Math.random() * 10000000}`, 10);
+
+        styles.push(
+          getBlockStyles({
+            css: o.css,
+            defaultStyles,
+            containerClassName: `__block-${o.cssId}`,
+            parentClassName: '',
+          })
+        );
+      }
+      return Object.entries<{ css?: string }>(o).reduce((prev, [k, v]) => {
+        return {
+          ...prev,
+          [k]: computeCss(v),
+        };
+      }, {});
+    }
+    return o;
+  }
   // @ts-ignore
   page.cssId = parseInt(Math.random() * 10000000, 10);
-  function computeBlocksStyles(blocks: any = []) {
-    if (!Array.isArray(blocks)) return blocks;
-    return blocks.map((block: any) => {
-      const config = block.config ? block.config : block;
-      config.cssId = parseInt(`${Math.random() * 10000000}`, 10);
-      const slug = block.slug || '';
-      const { styles: defaultStyles } = (builtinBlocks as any)[slug] || {
-        styles: '',
-      };
-
-      styles.push(
-        getBlockStyles({
-          css: config.css,
-          defaultStyles,
-          containerClassName: `__block-${config.cssId}`,
-          parentClassName: '',
-        })
-      );
-      if (config.blocks) {
-        config.blocks = computeBlocksStyles(config.blocks);
-      }
-      return block;
-    });
-  }
-  page.blocks = computeBlocksStyles(page.blocks || []);
+  page.blocks = computeCss(page.blocks);
 
   return { page, styles: styles.join('\n') };
 }
