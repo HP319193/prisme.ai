f688a5c94a764bbb39da32e0d24e820e11247e7b
Hadrien Lanneau <hadrien.lanneau@gogowego.com>
2023-05-12 14:02:49 +0200

    Reuse action schema form in header nav

--- services/console/components/PageBuilder/Panel/EditSchema/Header.tsx
+++ services/console/components/PageBuilder/Panel/EditSchema/Header.tsx
@@ -1,6 +1,7 @@
 import { builtinBlocks } from '@prisme.ai/blocks';
 import { Schema } from '@prisme.ai/design-system';
 import { getCommonSchema } from './commonSchema';
+import actionSchema from './Action'
 
 export const schema: Schema = {
   type: 'object',
@@ -126,97 +127,7 @@ export const schema: Schema = {
         description: 'pages.blocks.header.settings.nav.items.description',
         add: 'pages.blocks.header.settings.nav.items.add',
         remove: 'pages.blocks.header.settings.nav.items.remove',
-        properties: {
-          text: {
-            type: 'string',
-            title: 'pages.blocks.header.settings.nav.items.text.label',
-            description:
-              'pages.blocks.header.settings.nav.items.text.description',
-          },
-          type: {
-            title: 'pages.blocks.header.settings.nav.items.type.label',
-            oneOf: [
-              {
-                title: 'pages.blocks.header.settings.nav.items.external.label',
-                description:
-                  'pages.blocks.header.settings.nav.items.external.description',
-                value: 'external',
-                properties: {
-                  value: {
-                    type: 'string',
-                    title:
-                      'pages.blocks.header.settings.nav.items.external.value.label',
-                    description:
-                      'pages.blocks.header.settings.nav.items.external.value.description',
-                  },
-                },
-              },
-              {
-                title: 'pages.blocks.header.settings.nav.items.internal.label',
-                description:
-                  'pages.blocks.header.settings.nav.items.internal.description',
-                value: 'internal',
-                properties: {
-                  value: {
-                    type: 'string',
-                    title:
-                      'pages.blocks.header.settings.nav.items.internal.value.label',
-                    description:
-                      'pages.blocks.header.settings.nav.items.internal.value.description',
-                    'ui:widget': 'select',
-                    'ui:options': {
-                      from: 'pages',
-                    },
-                  },
-                },
-              },
-              {
-                title: 'pages.blocks.header.settings.nav.items.inside.label',
-                description:
-                  'pages.blocks.header.settings.nav.items.inside.description',
-                value: 'inside',
-                properties: {
-                  value: {
-                    type: 'string',
-                    title:
-                      'pages.blocks.header.settings.nav.items.inside.value.label',
-                    description:
-                      'pages.blocks.header.settings.nav.items.inside.value.description',
-                    'ui:widget': 'select',
-                    'ui:options': {
-                      from: 'pageSections',
-                    },
-                  },
-                },
-              },
-              {
-                title: 'pages.blocks.header.settings.nav.items.event.label',
-                description:
-                  'pages.blocks.header.settings.nav.items.event.description',
-                value: 'event',
-                properties: {
-                  value: {
-                    type: 'string',
-                    title:
-                      'pages.blocks.header.settings.nav.items.event.value.label',
-                    description:
-                      'pages.blocks.header.settings.nav.items.event.value.description',
-                    'ui:widget': 'autocomplete',
-                    'ui:options': {
-                      autocomplete: 'events:listen',
-                    },
-                  },
-                  payload: {
-                    title:
-                      'pages.blocks.header.settings.nav.items.event.payload.label',
-                    description:
-                      'pages.blocks.header.settings.nav.items.event.payload.description',
-                  },
-                },
-              },
-            ],
-          },
-        },
+        properties: actionSchema.properties,
       },
     },
     fixed: {
