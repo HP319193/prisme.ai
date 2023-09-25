4998e1ab9e4db3855a26ee3b0cf5a0c288f3562e
Hadrien Lanneau <hadrien.lanneau@gogowego.com>
2023-09-25 17:17:57 +0200

    Add indonesian, viet and thai language files and config

--- services/console/next-i18next.config.js
+++ services/console/next-i18next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   i18n: {
     defaultLocale: 'default',
-    locales: ['default', 'en', 'fr', 'es'],
-    supportedLngs: ['en', 'fr'],
+    locales: ['default', 'en', 'fr', 'es', 'id', 'th', 'vi'],
+    supportedLngs: ['en', 'fr', 'id', 'th', 'vi'],
     localeDetection: true,
   },
   reloadOnPrerender: process.env.NODE_ENV === 'development',
