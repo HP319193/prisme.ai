fc7dc80ead99d84cc76224e1c3a8e2b868790390
martin <martin.caussanel@prisme.ai>
2023-10-09 17:08:02 +0200

    Strip whitespaces in app slugs when injecting app configs from env vars

--- services/runtime/src/utils/extractConfigFromEnv.ts
+++ services/runtime/src/utils/extractConfigFromEnv.ts
@@ -1,4 +1,5 @@
 export function extractConfigFromEnv(type: 'workspace' | 'app', slug: string) {
+  slug = (slug || '').replace(/ /g, '');
   const prefix =
     type === 'app' ? `APP_CONFIG_${slug}_` : `WORKSPACE_CONFIG_${slug}_`;
   return Object.entries(process.env).reduce(
