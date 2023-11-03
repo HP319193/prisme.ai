6758938d1c3a198d69edf3d0b71fd9e69402cbbf
Hadrien Lanneau <hadrien.lanneau@gogowego.com>
2023-11-03 11:44:14 +0100

    Copy and paste blocks

--- services/console/utils/Copy.tsx
+++ services/console/utils/Copy.tsx
@@ -1,26 +1,12 @@
+import { TextArea } from '@prisme.ai/design-system';
+import { FormEvent, ReactElement } from 'react';
 import { render } from 'react-dom';
 
-function fallback(text: string) {
-  console.log(text);
-
+function fallback(child: ReactElement) {
   const d = document.createElement('dialog');
   d.classList.add('copy-fallback');
-  function selectAll() {
-    const selection = window.getSelection();
-    if (!selection) return;
-    const range = document.createRange();
-    range.selectNodeContents(d);
-    selection.removeAllRanges();
-    selection.addRange(range);
-  }
-  render(
-    <div className="p-4" onClick={selectAll}>
-      <pre>
-        <code>{text}</code>
-      </pre>
-    </div>,
-    d
-  );
+
+  render(child, d);
   document.body.appendChild(d);
   d.addEventListener('mousedown', function (e) {
     e.stopPropagation();
@@ -35,14 +21,66 @@ function fallback(text: string) {
     }
   });
   d.showModal();
-  selectAll();
+  return d;
+}
+
+function fallbackCopy(text: string) {
+  console.log(text);
+  function selectAll(d: Element) {
+    const selection = window.getSelection();
+    if (!selection) return;
+    const range = document.createRange();
+    range.selectNodeContents(d);
+    selection.removeAllRanges();
+    selection.addRange(range);
+  }
+  const d = fallback(
+    <div className="p-4" onClick={() => selectAll(d)}>
+      <pre>
+        <code>
+          {typeof text === 'string' ? text : JSON.stringify(text, null, '  ')}
+        </code>
+      </pre>
+    </div>
+  );
+  selectAll(d);
 }
 
 export function copy(text: string) {
   try {
     window.navigator.clipboard.writeText(text);
   } catch (e) {
-    fallback(text);
+    fallbackCopy(text);
+  }
+}
+
+function fallbackPaste(): Promise<string> {
+  return new Promise((resolve) => {
+    const d = fallback(
+      <form
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          padding: '1rem',
+        }}
+        onSubmit={(e: FormEvent<HTMLFormElement>) => {
+          e.preventDefault();
+          resolve(e.currentTarget.pasting.value);
+          d.close();
+        }}
+      >
+        <TextArea name="pasting" style={{ minWidth: '50vw' }} autoSize />
+        <button type="submit">Paste</button>
+      </form>
+    );
+  });
+}
+
+export async function paste() {
+  try {
+    return await window.navigator.clipboard.readText();
+  } catch {
+    return await fallbackPaste();
   }
 }
 
