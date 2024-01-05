cd0b333daaf1cd482fcf3ceefa7cae705882ee40
Hadrien Lanneau <hadrien.lanneau@gogowego.com>
2024-01-05 14:44:46 +0100

    Fix links in datatable actions

--- packages/blocks/lib/Blocks/DataTable/RenderValue.tsx
+++ packages/blocks/lib/Blocks/DataTable/RenderValue.tsx
@@ -5,6 +5,7 @@ import { ColumnDefinition } from './types';
 import { interpolate } from '../../interpolate';
 import { useBlock } from '../../Provider';
 import { RichText } from '../RichText';
+import { useBlocks } from '../../Provider/blocksContext';
 
 const generateColor = (str: string) => {
   const cyrb53 = function (str = '', seed = 0) {
@@ -32,104 +33,105 @@ interface RenderValueAttributes extends ColumnDefinition {
   language: string;
 }
 
-export const renderValue = ({
-  key,
-  type,
-  language,
-  format,
-  onEdit,
-  actions,
-}: RenderValueAttributes) => (_: any, item: any) => {
-  const value = key ? item[key] : undefined;
-  const { events } = useBlock();
+export const renderValue =
+  ({ key, type, language, format, onEdit, actions }: RenderValueAttributes) =>
+  (_: any, item: any) => {
+    const value = key ? item[key] : undefined;
+    const { events } = useBlock();
+    const {
+      components: { Link },
+    } = useBlocks();
 
-  switch (type) {
-    case 'number': {
-      try {
-        const formatter = new Intl.NumberFormat(
-          language,
-          format as Intl.NumberFormatOptions
-        );
-        return formatter.format(+value);
-      } catch {
-        return +value;
+    switch (type) {
+      case 'number': {
+        try {
+          const formatter = new Intl.NumberFormat(
+            language,
+            format as Intl.NumberFormatOptions
+          );
+          return formatter.format(+value);
+        } catch {
+          return +value;
+        }
       }
-    }
-    case 'date': {
-      try {
-        const formatter = new Intl.DateTimeFormat(
-          language,
-          format as Intl.DateTimeFormatOptions
-        );
-        return formatter.format(new Date(value));
-      } catch {
-        return value;
+      case 'date': {
+        try {
+          const formatter = new Intl.DateTimeFormat(
+            language,
+            format as Intl.DateTimeFormatOptions
+          );
+          return formatter.format(new Date(value));
+        } catch {
+          return value;
+        }
       }
-    }
-    case 'boolean':
-      return <Switch checked={!!value} disabled={!onEdit} />;
-    case 'tags':
-      const tags = Array.isArray(value) ? value : [value];
+      case 'boolean':
+        return <Switch checked={!!value} disabled={!onEdit} />;
+      case 'tags':
+        const tags = Array.isArray(value) ? value : [value];
 
-      return (
-        <>
-          {tags.map((tag) => (
-            <Tag
-              color={generateColor(tag)}
-              key={tag}
-              style={{
-                color: Color(generateColor(tag)).isLight()
-                  ? 'inherit'
-                  : 'white',
-              }}
-            >
-              {tag}
-            </Tag>
-          ))}
-        </>
-      );
-    case 'string':
-    default:
-      if (actions && Array.isArray(actions) && actions.length > 0) {
         return (
           <>
-            {actions.map(
-              ({ label, action: { type, value, payload, popup } = {} }) => (
-                <Button
-                  key={`${label}${type}${value}`}
-                  type="button"
-                  onClick={() => {
-                    if (!value) return;
-                    const { key, ...data } = item;
-                    if (type === 'event') {
-                      events?.emit(value, {
-                        ...interpolate(payload, data),
-                        data,
-                        key,
-                      });
-                    }
-                    if (type === 'url') {
-                      const href = interpolate(value, data);
-                      if (popup) {
-                        window.open(href);
-                      } else {
-                        window.location = href;
-                      }
-                    }
-                  }}
-                >
-                  {label}
-                </Button>
-              )
-            )}
+            {tags.map((tag) => (
+              <Tag
+                color={generateColor(tag)}
+                key={tag}
+                style={{
+                  color: Color(generateColor(tag)).isLight()
+                    ? 'inherit'
+                    : 'white',
+                }}
+              >
+                {tag}
+              </Tag>
+            ))}
           </>
         );
-      }
-      if (typeof value === 'object') {
-        return JSON.stringify(value);
-      }
+      case 'string':
+      default:
+        if (actions && Array.isArray(actions) && actions.length > 0) {
+          return (
+            <>
+              {actions.map(
+                ({ label, action: { type, value, payload, popup } = {} }) => {
+                  if (type === 'event') {
+                    return (
+                      <Button
+                        key={`${label}${type}${value}`}
+                        type="button"
+                        onClick={() => {
+                          if (!value) return;
+                          const { key, ...data } = item;
+                          events?.emit(value, {
+                            ...interpolate(payload, data),
+                            data,
+                            key,
+                          });
+                        }}
+                      >
+                        {label}
+                      </Button>
+                    );
+                  }
+                  if (type === 'url') {
+                    const { key, ...data } = item;
+                    const href = interpolate(value, data);
+                    return (
+                      <Link href={href} target={popup ? '_blank' : undefined}>
+                        {label}
+                      </Link>
+                    );
+                  }
+                }
+              )}
+            </>
+          );
+        }
+        if (typeof value === 'object') {
+          return JSON.stringify(value);
+        }
 
-      return <RichText>{value}</RichText>;
-  }
-};
+        return <RichText>{value}</RichText>;
+    }
+  };
 export default renderValue;
