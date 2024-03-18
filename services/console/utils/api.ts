2308c7eb0e82bc4bfe1825855a7144d7bb63c5c4
Hadrien Lanneau <hadrien.lanneau@gogowego.com>
2024-03-18 11:26:15 +0100

    Avoid crash when console url is shit

--- services/console/utils/api.ts
+++ services/console/utils/api.ts
@@ -3,16 +3,24 @@ import { Api } from '@prisme.ai/sdk';
 import Storage from './Storage';
 
 const { publicRuntimeConfig } = getConfig();
+
+function getRedirectURI() {
+  try {
+    return new URL(
+      '/signin',
+      publicRuntimeConfig.CONSOLE_URL || 'http://localhost:3000'
+    ).toString();
+  } catch {
+    return '';
+  }
+}
 const api = new Api({
   host: publicRuntimeConfig.API_URL,
   oidc: {
     url: publicRuntimeConfig.OIDC_PROVIDER_URL,
     clientId: publicRuntimeConfig.OIDC_STUDIO_CLIENT_ID,
     clientIdHeader: publicRuntimeConfig.OIDC_CLIENT_ID_HEADER,
-    redirectUri: new URL(
-      '/signin',
-      publicRuntimeConfig.CONSOLE_URL || 'http://localhost:3000'
-    ).toString(),
+    redirectUri: getRedirectURI(),
   },
 });
 api.token = Storage.get('access-token');
