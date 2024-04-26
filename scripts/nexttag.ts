b3f36487201d021ba09c871764875a8121739f9f
Hadrien Lanneau <hadrien.lanneau@gogowego.com>
2024-04-26 12:04:07 +0200

    Update package.json when creating a new version

--- scripts/nexttag.ts
+++ scripts/nexttag.ts
@@ -1,4 +1,5 @@
 const date = new Date(Date.now());
+import fs from 'fs';
 
 const major = Array.from(`${date.getFullYear()}`).splice(2).join('');
 const minor = `${date.getMonth() + 1}`.padStart(2, '0');
@@ -7,5 +8,12 @@ const patch =
   `${date.getHours()}`.padStart(2, '0') +
   `${date.getMinutes()}`.padStart(2, '0');
 
-process.stdout.write(`v${major}.${minor}.${patch}`);
+const version = `${major}.${minor}.${patch}`;
+const pkg = JSON.parse(`${fs.readFileSync(`${__dirname}/../package.json`)}`);
+pkg.version = version;
+fs.writeFileSync(
+  `${__dirname}/../package.json`,
+  JSON.stringify(pkg, null, '  ')
+);
+process.stdout.write(`v${version}`);
 process.exit();
