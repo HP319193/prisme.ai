cf2e9c84e56b39d432c203b21b8385b7ace4c94e
Hadrien Lanneau <hadrien.lanneau@gogowego.com>
2024-05-06 16:38:20 +0200

    Tests AI Inbox : adding and removing channels

--- tests-e2e/products/ai-inbox.spec.ts
+++ tests-e2e/products/ai-inbox.spec.ts
@@ -31,84 +31,195 @@ test.beforeEach(async ({ page }) => {
   await page.waitForURL(baseUrl);
   await page.context().storageState({ path: authFile });
 });
-test('Display AI Inbox', async ({ page }) => {
-  await page.goto(baseUrl);
 
-  await expect(
-    page.getByTestId('schema-form-field-values.search')
-  ).toBeAttached();
+test.describe('Teams list', () => {
+  test('Display AI Inbox', async ({ page }) => {
+    await page.goto(baseUrl);
 
-  await expect(
-    page.getByRole('button', { name: 'Créer une équipe' })
-  ).toBeAttached();
-});
+    await expect(
+      page.getByTestId('schema-form-field-values.search')
+    ).toBeAttached();
 
-test('Create a team', async ({ page }) => {
-  await page.goto(baseUrl);
-
-  await page.getByRole('button', { name: 'Créer une équipe' }).click();
-
-  await page
-    .getByTestId('schema-form-field-values.name')
-    .fill('name new team for test');
-  await page
-    .getByTestId('schema-form-field-values.description')
-    .fill('description new team for test');
-
-  const fileChooserPromise = page.waitForEvent('filechooser');
-  await page.getByTestId('schema-form-field-values.img').click();
-  const fileChooser = await fileChooserPromise;
-  await fileChooser.setFiles(path.join(__dirname, '../assets/prisme-logo.png'));
-  await page.getByTestId('schema-form-field-values.name').press('Enter');
-  let createdId = '';
-  await page.waitForURL((url: URL) => {
-    if (url.pathname === '/fr/inbox') {
-      createdId = url.searchParams.get('team') || '';
-      return true;
-    }
-    return false;
+    await expect(
+      page.getByRole('button', { name: 'Créer une équipe' })
+    ).toBeAttached();
   });
-  await expect(page.getByText('name new team for test').nth(1)).toBeAttached();
-  await expect(page.getByText('description new team for test')).toBeAttached();
-  await expect(
-    page.locator('span').filter({ hasText: 'name new team for test' })
-  ).toBeAttached();
-  await page
-    .locator('div')
-    .filter({ hasText: /^Nname new team for test$/ })
-    .getByRole('link')
-    .click();
-  await page.waitForURL(baseUrl);
-  await expect(
-    page.locator(`[href="/fr/inbox?team=${createdId}"]`)
-  ).toBeAttached();
 
-  // delete
-  await page.goto(`${baseUrl}/deleteTeam?team=${createdId}`);
+  test('Create a team', async ({ page }) => {
+    await page.goto(baseUrl);
+
+    await page.getByRole('button', { name: 'Créer une équipe' }).click();
+
+    await page
+      .getByTestId('schema-form-field-values.name')
+      .fill('name new team for test');
+    await page
+      .getByTestId('schema-form-field-values.description')
+      .fill('description new team for test');
+
+    const fileChooserPromise = page.waitForEvent('filechooser');
+    await page.getByTestId('schema-form-field-values.img').click();
+    const fileChooser = await fileChooserPromise;
+    await fileChooser.setFiles(
+      path.join(__dirname, '../assets/prisme-logo.png')
+    );
+    await page.getByTestId('schema-form-field-values.name').press('Enter');
+    let createdId = '';
+    await page.waitForURL((url: URL) => {
+      if (url.pathname === '/fr/inbox') {
+        createdId = url.searchParams.get('team') || '';
+        return true;
+      }
+      return false;
+    });
+    await expect(
+      page.getByText('name new team for test').nth(1)
+    ).toBeAttached();
+    await expect(
+      page.getByText('description new team for test')
+    ).toBeAttached();
+    await expect(
+      page.locator('span').filter({ hasText: 'name new team for test' })
+    ).toBeAttached();
+    await page
+      .locator('div')
+      .filter({ hasText: /^Nname new team for test$/ })
+      .getByRole('link')
+      .click();
+    await page.waitForURL(baseUrl);
+    await expect(
+      page.locator(`[href="/fr/inbox?team=${createdId}"]`)
+    ).toBeAttached();
+
+    // delete
+    await page.goto(`${baseUrl}/deleteTeam?team=${createdId}`);
+  });
+
+  test('Search a team', async ({ page }) => {
+    const framesSent: string[] = [];
+    page.on('websocket', (ws) => {
+      ws.on('framesent', (event) => {
+        framesSent.push(event.payload.toString());
+      });
+    });
+
+    await page.goto(baseUrl);
+    await page.getByTestId('schema-form-field-values.search').focus();
+    await page.getByTestId('schema-form-field-values.search').fill('équipe');
+    await page.getByTestId('schema-form-field-values.search').focus();
+    await page.waitForTimeout(1000);
+    await page.keyboard.press('Enter');
+    await expect(framesSent).toContain(
+      '42/v2/workspaces/iGsXZ6I/events,["event",{"type":"filter teams","payload":{"search":"équipe"}}]'
+    );
+    await expect(
+      page.getByRole('button', { name: 'Une équipe Une équipe' })
+    ).toBeAttached();
+    await expect(
+      page.getByRole('button', { name: 'Une autre équipe' })
+    ).toBeAttached();
+    await expect(
+      page.getByRole('button', { name: 'A Team' })
+    ).not.toBeAttached();
+  });
 });
 
-test('Search a team', async ({ page }) => {
-  const framesSent: string[] = [];
-  page.on('websocket', (ws) => {
-    ws.on('framesent', (event) => {
-      framesSent.push(event.payload.toString());
+test.describe('Channels', () => {
+  let createdId = '';
+  test.beforeEach(async ({ page }) => {
+    await page.getByRole('button', { name: 'Créer une équipe' }).click();
+
+    await page
+      .getByTestId('schema-form-field-values.name')
+      .fill('name testing channels');
+    await page
+      .getByTestId('schema-form-field-values.description')
+      .fill('description testing channels');
+    await page.getByTestId('schema-form-field-values.name').press('Enter');
+    createdId = '';
+    await page.waitForURL((url: URL) => {
+      if (url.pathname === '/fr/inbox') {
+        createdId = url.searchParams.get('team') || '';
+        return true;
+      }
+      return false;
     });
   });
+  test.afterEach(async ({ page }) => {
+    if (!createdId) return;
+    await page.goto(`${baseUrl}/deleteTeam?team=${createdId}`);
+  });
 
-  await page.goto(baseUrl);
-  await page.getByTestId('schema-form-field-values.search').focus();
-  await page.getByTestId('schema-form-field-values.search').fill('équipe');
-  await page.getByTestId('schema-form-field-values.search').focus();
-  await page.waitForTimeout(1000);
-  await page.keyboard.press('Enter');
-  await expect(framesSent).toContain(
-    '42/v2/workspaces/iGsXZ6I/events,["event",{"type":"filter teams","payload":{"search":"équipe"}}]'
-  );
-  await expect(
-    page.getByRole('button', { name: 'Une équipe Une équipe' })
-  ).toBeAttached();
-  await expect(
-    page.getByRole('button', { name: 'Une autre équipe' })
-  ).toBeAttached();
-  await expect(page.getByRole('button', { name: 'A Team' })).not.toBeAttached();
+  test('List empty channels', async ({ page }) => {
+    await page.goto(baseUrl);
+
+    await page.getByRole('link', { name: 'name testing channels' }).click();
+
+    await page.getByRole('button', { name: 'Canaux Canaux' }).click();
+
+    await expect(
+      page.getByRole('cell', { name: 'Aucune donnée' })
+    ).toBeAttached();
+  });
+
+  test('Add channels', async ({ page }) => {
+    await page.goto(baseUrl);
+
+    await page.getByRole('link', { name: 'name testing channels' }).click();
+
+    await page.getByRole('button', { name: 'Canaux Canaux' }).click();
+
+    // Add a new canal
+    await page
+      .getByRole('button', { name: 'Ajouter un nouveau canal' })
+      .click();
+    await page.getByLabel('Projet AI Knowledge').click();
+    await page.getByText('Projet de test').click();
+    await page.getByRole('button', { name: 'Envoyer' }).click();
+    await expect(
+      page.getByRole('cell', { name: 'Projet de test' })
+    ).toBeAttached();
+    await expect(page.getByText('Le canal a bien été ajouté')).toBeAttached();
+
+    // Try to add it again
+    await page
+      .getByRole('button', { name: 'Ajouter un nouveau canal' })
+      .click();
+    await page.getByLabel('Projet AI Knowledge').click();
+    await page
+      .locator('.ant-select-dropdown')
+      .getByText('Projet de test')
+      .click();
+    await page.getByRole('button', { name: 'Envoyer' }).click();
+
+    await expect(page.getByText('Ce canal existe déjà.')).toBeAttached();
+  });
+
+  test('Delete channels', async ({ page }) => {
+    await page.goto(baseUrl);
+
+    await page.getByRole('link', { name: 'name testing channels' }).click();
+
+    await page.getByRole('button', { name: 'Canaux Canaux' }).click();
+
+    // Add a new canal
+    await page
+      .getByRole('button', { name: 'Ajouter un nouveau canal' })
+      .click();
+    await page.getByLabel('Projet AI Knowledge').click();
+    await page.getByText('Projet de test').click();
+    await page.getByRole('button', { name: 'Envoyer' }).click();
+    await expect(
+      page.getByRole('cell', { name: 'Projet de test' })
+    ).toBeAttached();
+    await expect(page.getByText('Le canal a bien été ajouté')).toBeAttached();
+
+    // Try to add it again
+    await page.getByRole('button', { name: 'Supprimer le canal' }).click();
+    await page.getByRole('button', { name: 'Oui' }).click();
+
+    await expect(
+      page.getByRole('cell', { name: 'Aucune donnée' })
+    ).toBeAttached();
+  });
 });
