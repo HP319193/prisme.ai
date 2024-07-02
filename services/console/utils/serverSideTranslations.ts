67608d11a7051b389dd348c61857f1a2bbf6fd02
Hadrien Lanneau <hadrien.lanneau@gogowego.com>
2024-07-02 10:09:49 +0200

    Fix caching translations

--- services/console/utils/serverSideTranslations.ts
+++ services/console/utils/serverSideTranslations.ts
@@ -9,43 +9,49 @@ const {
 let promise: Promise<any> | undefined;
 let isNotFound: true | undefined;
 
+function fetchOverrideTranslations() {
+  promise =
+    promise && process?.env?.NODE_ENV !== 'development'
+      ? promise
+      : new Promise(async (resolve) => {
+          /**
+           * It will not retry loading translations if a 404 has been returned in last 60s
+           * Then, the response will be kept in cache for 60s
+           */
+          if (!isNotFound && TRANSLATIONS_OVERRIDE) {
+            try {
+              const response = await fetch(TRANSLATIONS_OVERRIDE);
+              if (response.status === 404) {
+                isNotFound = true;
+                resolve({});
+                return;
+              }
+              const override = await response.json();
+              resolve(override);
+            } catch (e) {
+              console.error('TRANSLATIONS_OVERRIDE is invalid', e);
+              resolve({});
+            }
+          }
+          setTimeout(() => {
+            promise = undefined;
+            isNotFound = undefined;
+          }, 60 * 1000);
+        });
+}
+
 export const serverSideTranslations: typeof originalServerSideTranslations =
   async (initialLocale, namespacesRequired, configOverride, extraLocales) => {
-    if (promise && process?.env?.NODE_ENV !== 'development') return promise;
-    promise = new Promise(async (resolve) => {
-      const translations = await originalServerSideTranslations(
-        initialLocale,
-        namespacesRequired,
-        configOverride,
-        extraLocales
-      );
-
-      /**
-       * It will not retry loading translations if a 404 has been returned in last 60s
-       * Then, the response will be kept in cache for 60s
-       */
-      if (!isNotFound && TRANSLATIONS_OVERRIDE) {
-        try {
-          const response = await fetch(TRANSLATIONS_OVERRIDE);
-          if (response.status === 404) {
-            isNotFound = true;
-            resolve(translations);
-            return;
-          }
-          const override = await response.json();
-          translations._nextI18Next.initialI18nStore = merge(
-            translations._nextI18Next.initialI18nStore,
-            override
-          );
-        } catch (e) {
-          console.error('TRANSLATIONS_OVERRIDE is invalid', e);
-        }
-      }
-      setTimeout(() => {
-        promise = undefined;
-        isNotFound = undefined;
-      }, 60 * 1000);
-      resolve(translations);
-    });
-    return promise;
+    const translations = await originalServerSideTranslations(
+      initialLocale,
+      namespacesRequired,
+      configOverride,
+      extraLocales
+    );
+    const override = await fetchOverrideTranslations();
+    translations._nextI18Next.initialI18nStore = merge(
+      translations._nextI18Next.initialI18nStore,
+      override
+    );
+    return translations;
   };
